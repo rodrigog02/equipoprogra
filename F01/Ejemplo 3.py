ac15a2097d14a164c5a19a18e109fc3838068a09
DarkZodiac0 <axel0030105@hotmail.com>
2025-03-03 23:45:20 -0600

    Update Ejemplo 3.py
    codigo terminado

--- F01/Ejemplo 3.py
+++ F01/Ejemplo 3.py
@@ -1,1 +1,10 @@
+# Solicita al usuario que ingrese un número y lo convierte a entero
+numero = int(input("Ingrese un número: "))
 
+# Verifica si el número es par o impar
+if numero % 2 == 0:
+    # Si el residuo de la división entre 2 es 0, imprime que el número es par
+    print("El número es par")
+else:
+    # Si el residuo no es 0, imprime que el número es impar
+    print("El número es impar")
