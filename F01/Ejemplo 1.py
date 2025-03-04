f8fa7e718c59aa9c72e5589f9a0bf7dee5eb82d3
KJesus28 <karolj.cruz911@gmail.com>
2025-03-03 23:06:03 -0600

    Update Ejemplo 1.py

--- F01/Ejemplo 1.py
+++ F01/Ejemplo 1.py
@@ -1,1 +1,19 @@
+# EquipoProgra
+
+# Este código define seis variables, dos son ocupadas como datos y cuatro como resultados de operaciones básicas para después imprimirse en la consola con la función print
+# 2025/03/03 - V.1.0.0 se agregan comentarios y encabezado al código
+#Trabajó: Karol Jesus (KJesús28) 
+#Python permite trabajar con variables sin necesidad de especificar el tipo de dato explícitamente.
+valor1 = 7 #se define valor1 con el valor de 7
+valor2 = 4 #se define valor2 con el valor de 4
+
+suma = valor1 + valor2 #se define suma con el resultado de la suma de valor1 y valor2
+resta = valor1 - valor2 #se define resta con el resultado de la resta de valor1 menos valor2
+producto = valor1 * valor2 #se define producto con el resultado de la multiplicación de valor1 por valor2
+division = valor1 / valor2 #se define division con el resultado de la división de valor1 entre valor2
+
+print("Suma:", suma) #imprime "Suma:" seguido del valor de suma
+print("Resta:", resta) #imprime "Resta:" seguido del valor de resta
+print("Producto:", producto) #imprime "Producto:" seguido del valor de producto
+print("División:", division) #imprime "División:" seguido del valor de division
 
