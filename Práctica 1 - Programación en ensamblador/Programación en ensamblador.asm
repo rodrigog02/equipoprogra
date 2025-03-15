aed10d35a6921ae993f14bda4ad77c5c9073ee0c
rodrigog02 <zs23013937@estudiantes.uv.mx>
2025-03-14 22:24:13 -0600

    Update Programación en ensamblador.asm
    se agrego codigo

--- Práctica 1 - Programación en ensamblador/Programación en ensamblador.asm
+++ Práctica 1 - Programación en ensamblador/Programación en ensamblador.asm
@@ -1,1 +1,13 @@
+JMP boot
+
+stackTop    EQU 0xFF          ; Puntero de pila
+textDisplay EQU 0x2E0         ; Dirección de la pantalla de texto
+gfxDisplay  EQU 0x300         ; Dirección base de la pantalla gráfica
+
+; Definir posiciones para la animación (dos frames)
+frame0      EQU 0x300         ; Posición 1
+frame1      EQU 0x304         ; Posición 2 (desplazada 4 bytes a la derecha)
+
+hello:      DB "Hola profe!"
+            DB 0              ; Terminador de cadena
 
