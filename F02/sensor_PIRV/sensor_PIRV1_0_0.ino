c1e0880661bc2af50c8fcdc5e6c82e8abac8f625
KJesus28 <karolj.cruz911@gmail.com>
2025-03-28 23:07:06 -0600

    Update sensor_PIRV1_0_0.ino

--- F02/sensor_PIRV/sensor_PIRV1_0_0.ino
+++ F02/sensor_PIRV/sensor_PIRV1_0_0.ino
@@ -1,1 +1,20 @@
+int sensorState = 0;
 
+void setup()
+{
+  pinMode(2, INPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop()
+{
+  
+  sensorState = digitalRead(2);
+  
+  if (sensorState == HIGH) {
+    digitalWrite(LED_BUILTIN, HIGH);
+  } else {
+    digitalWrite(LED_BUILTIN, LOW);
+  }
+  delay(10); 
+}
