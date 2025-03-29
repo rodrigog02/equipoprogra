2dfa47f9d8fc23b6c809e9dba65416e2e153a2c0
KJesus28 <karolj.cruz911@gmail.com>
2025-03-28 23:08:33 -0600

    Update Sensor_ultrasonicoV1_0_0.ino

--- F02/Sensor_ultrasonico/Sensor_ultrasonicoV1_0_0.ino
+++ F02/Sensor_ultrasonico/Sensor_ultrasonicoV1_0_0.ino
@@ -1,1 +1,63 @@
+const int pingPin = 7;
+const int red=11;
+const int blue=10;
+int green=9;
+void setup() {
+  
+  Serial.begin(9600);
+  pinMode(red,OUTPUT);
+  pinMode(blue,OUTPUT);
+  pinMode(green,OUTPUT);
+}
 
+void loop() {
+ 
+  long duration, inches, cm;
+
+
+  pinMode(pingPin, OUTPUT);
+  digitalWrite(pingPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(pingPin, HIGH);
+  delayMicroseconds(5);
+  digitalWrite(pingPin, LOW);
+
+ 
+  pinMode(pingPin, INPUT);
+  duration = pulseIn(pingPin, HIGH);
+
+  inches = microsecondsToInches(duration);
+  cm = microsecondsToCentimeters(duration);
+
+  Serial.print(inches);
+  Serial.print("in, ");
+  Serial.print(cm);
+  Serial.print("cm");
+  Serial.println();
+  
+  if(inches<10){
+    digitalWrite(red,HIGH);
+    digitalWrite(green,LOW);
+    digitalWrite(blue,LOW);
+  }
+  else if (inches>10 && inches<50){
+    digitalWrite(red,LOW);
+    digitalWrite(green,LOW);
+    digitalWrite(blue,HIGH); 
+  }
+  
+  else{ 
+    digitalWrite(red,LOW);
+    digitalWrite(green,HIGH);
+    digitalWrite(blue,LOW); 
+  }
+}
+long microsecondsToInches(long microseconds) {
+
+  return microseconds / 74 / 2;
+}
+
+long microsecondsToCentimeters(long microseconds) {
+  
+  return microseconds / 29 / 2;
+}
